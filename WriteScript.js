d3d0f871ec4f7614b60d6e49624b2ba664742fd3
Pete Rossman <pete.rossman@gmail.com>
2017-04-24 16:43:31 -0400

    Fixed bug when summing totals, made cosmetic changes including textarea

--- WriteScript.js
+++ WriteScript.js
@@ -1,5 +1,5 @@
 
-var sum = 0;
+
 var cartArray = [0 , 0, 0, 0];
 function GetSelectedItem(el)
 {
@@ -9,26 +9,33 @@ function GetSelectedItem(el)
 	cartArray[e.value]++;
 }
 
-function checkout(food){	
+function checkout(food){
+	var sum = 0;
 	for (var i = 0; i<food.length; i++){
 		if (food[i] > 0){
-			document.write("<br>You bought " + food[i] + " " + items[i] + " at a cost of $" + (prices[i] * food[i]));
+			var writePrice = (prices[i] * food[i]).toFixed(2);
+			document.write("<br>You bought " + food[i] + " " + items[i] + " at a cost of $" + writePrice);
 			sum += (prices[i] * food[i]);
 		}		
 	}
 	var writeSum = sum.toFixed(2);
-	document.write("<br>Your total is: $" + writeSum + ". <br>Please come again!");
+	document.write("<br>Your total is: $" + writeSum + ". <br>Please come again!<br>");
 	document.write('<a href="file:///C:/Users/peter/OneDrive/Documents/ShoppingList/index.html">Go back to the store</a>');
 }
 
 function viewCart(food){
+	var sum = 0;
 	var theTextBox = document.getElementById("textbox");
 	theTextBox.value = "";
 	for (var i = 0; i < cartArray.length; i++){
 		if (cartArray[i] > 0){
-		(theTextBox.value += "You have " + food[i] + " " + items[i] + " at a cost of $" + (prices[i] * food[i]));
+		var writePrice = (prices[i] * food[i]).toFixed(2);
+		(theTextBox.value += "You have " + food[i] + " " + items[i] + " at a cost of $" + writePrice + "\n");
+		sum += (prices[i] * food[i]);
 		}
-	}	
+	}
+		var writeSum = sum.toFixed(2);
+		theTextBox.value += "\nYour total is: $" + writeSum + ".";
     
 }
 
