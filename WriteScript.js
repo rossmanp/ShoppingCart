1fc2bd0a996a0bf5b189a85d803481e12b9711ed
Pete Rossman <pete.rossman@gmail.com>
2017-04-24 15:17:26 -0400

    Add links in viewCart and Checkout functions to return to store

--- WriteScript.js
+++ WriteScript.js
@@ -24,10 +24,20 @@ function checkout(food){
 	}
 	var writeSum = sum.toFixed(2);
 	document.write("<br>Your total is: $" + writeSum + ". <br>Please come again!");
+	document.write('<a href="file:///C:/Users/peter/OneDrive/Documents/ShoppingList/index.html">Go back to the store</a>');
+}
+
+function viewCart(food){
+	for (var i = 0; i < cartArray.length; i++){
+		if (cartArray[i] > 0){
+		document.write("<br>You bought " + food[i] + " " + items[i] + " at a cost of $" + (prices[i] * food[i]));
+		}
+	}
+document.write('<a href="file:///C:/Users/peter/OneDrive/Documents/ShoppingList/index.html">Go back to the store</a>');
 }
 
 function resetCart(){
 	confirm("Do you want to empty your cart?");
 	alert("You have just emptied your cart.");
-	cartArray = [0 , 0, 0, 0];
+	cartArray = [0 , 0, 0, 0];	
 }
