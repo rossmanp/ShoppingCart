5aab40eea7cbe6a9322fba15ab0541618a9ffea0
Pete Rossman <pete.rossman@gmail.com>
2017-04-24 14:22:13 -0400

    Items and sum always show price to two decimal places

--- WriteScript.js
+++ WriteScript.js
@@ -1,6 +1,7 @@
 var sum = 0;
 		for (var i = 0; i < items.length; i++ ){ 
-	document.write("<br>" + items[i] + ": $" + prices[i] );	
-	sum += prices[i];
+	document.write("<br>" + items[i] + ": $" + printPrices[i] );	
+	sum += prices[i];	
 }	
-document.write("<br> The total price of the items is: " + sum + ".");
+var writeSum = sum.toFixed(2);
+document.write("<br> The total price of the items is: " + writeSum + ".");
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
   var items = ["Eggs", "Milk", "Bread", "Cheese"];
   var prices = [1.25,2.50,3.10,4.00];
+  var printPrices = []
   var sel = document.getElementById('sel1');
 	for(var i = 0; i < items.length; i++) {
     var opt = document.createElement('option');
-    opt.innerHTML = items[i] + ": $" + prices[i];
+	printPrices[i] = prices[i].toFixed(2);
+    opt.innerHTML = items[i] + ": $" + printPrices[i];
     opt.value = prices[i];
     sel.append(opt);
 }
